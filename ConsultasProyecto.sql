173824a17d562ed66c3470ab5fba2b5ab893977b
MariaGuadalepeRamirezG <71472812+MariaGuadalepeRamirezG@users.noreply.github.com>
2022-05-23 11:24:22 -0500

    Update ConsultasProyecto.sql

--- ConsultasProyecto.sql
+++ ConsultasProyecto.sql
@@ -1,4 +1,5 @@
 use northwind_dt;
+
 -- Reporte 1
 -- En un periodo indicado de 1 año, desglosar por trimestre, cuales fueron los productos
 -- dentro del top 5, considerando las unidades vendidas.
@@ -14,7 +15,7 @@ limit 5;
 -- Reporte 2
 -- Para un producto elegido y un periodo indicado, mostrar el desglose por mes del
 -- comportamiento en ventas de dicho producto.
-select p.productName, p.UnitPrice, md.Quantity, m.Date Mes
+select p.productName, p.UnitPrice, md.Quantity, m.Date MesJunio
 from Products p
 join movementdetails md on p.productid = md.productid
 join movements m on m.movementid = md.movementid
@@ -22,25 +23,15 @@ where month(m.Date) = '06'
 order by md.Quantity desc
 limit 10;
 
+
 -- Reporte 3
--- Mostrar los 10 producto que hay más en los almacenes al igual de los que hay menos
--- de tres almacenes por lo menos
+-- Mostrar los 5 producto que hay menos en los almacenes
+-- de la compañia 1
 
-select p.productName, w.Description, wp.UnitsInStock
+select p.productName, p.UnitPrice, w.Description, wp.UnitsInStock
 from products p
 join warehouseproducts wp on p.productId = wp.productId
 join warehouses w on w.warehouseID = wp.warehouseID
-where w.warehouseID  between 1 and 3 and 4
-order by wp.UnitsInStock desc
-limit 5;
-select p.productName, w.Description, wp.UnitsInStock
-from products p
-join warehouseproducts wp on p.productId = wp.productId
-join warehouses w on w.warehouseID = wp.warehouseID
-where wp.warehouseID  between 1 and 3 and 4
+where w.companyID  = 1
 order by wp.UnitsInStock asc
-limit 5;
-
-
-
-
+limit 15;
